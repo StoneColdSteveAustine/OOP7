253c19ff231c8efef1cd03b7dbb53167fb81db3c
StoneColdSteveAustine <115403281+StoneColdSteveAustine@users.noreply.github.com>
2023-08-01 20:24:00 +0600

    Create Calculable.java

---
+++ Calculable.java
@@ -0,0 +1,9 @@
+package OOP7;
+
+public interface Calculable {
+    Calculable sum(int argReal, int argImaginary);
+
+    Calculable multi(int argReal, int argImaginary);
+    Calculable div (int argReal, int argImaginary);
+    String getResult();
+}
